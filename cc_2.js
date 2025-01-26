95ec4e34c30f387a51ff32ba3fc007e6510faaba
Jasonmmill <jasonmatthewmill@gmail.com>
2025-01-26 00:54:39 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,10 @@
+// Task 1: Working with Arrays
+let products = [ "iPod", "iPad", "iPhone", "AirPods", "Macbook"]
+console.log(products)
+// add product
+products.push("Apple Watch")
+console.log(products)
+// remove product
+let removed = products.pop()
+console.log(removed)
+console.log(products)
